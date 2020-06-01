e3e44f2c73e98474cea973b826f75cc8f8537545
Jack Christopher Gammon <spiffyRAM@gmail.com>
2020-06-01 22:00:34 +1200

    fix indent

--- system-scripts/mount_ram_drive.sh
+++ system-scripts/mount_ram_drive.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 if [ ! -d "/home/pi/org" ]; then
-mkdir /home/pi/org
-mkfs -q  /dev/ram1 2048
-mount -y /dev/ram1 /home/pi/org
-chown pi /home/pi/org
-chown pi /dev/ram1
+    mkdir /home/pi/org
+    mkfs -q  /dev/ram1 2048
+    mount -y /dev/ram1 /home/pi/org
+    chown pi /home/pi/org
+    chown pi /dev/ram1
 fi
